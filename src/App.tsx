87533870c7e02fa5e3fa7c8b04f27d1b5bdfc424
Mentorstvo <mentorstvo>
2024-04-25 21:06:06 +0200

    fix: fix syles for header footer and main tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,51 +3,59 @@ import StyledLink from './components/ui/StyledLink'
 
 function App() {
   return (
-    <>
-      <div className="bg-slate-500">
-        <div className="flex justify-center">
-          <header className="fixed top-0">
-            <nav>
-              <ul className="flex gap-3">
-                <li>
-                  <StyledLink href="/home">Home</StyledLink>
-                </li>
-                <li>
-                  <StyledLink href="/store">Store</StyledLink>
-                </li>
-                <li>
-                  <StyledLink href="/page">Page</StyledLink>
-                </li>
-                <ShoppingBag href="/cart" className="hover:bg-gray-400" />
-              </ul>
-            </nav>
-          </header>
-        </div>
-
-        <div className="md:container md:mx-auto">
-          <main>
-            <section className="flex h-screen flex-col items-center gap-20 md:container md:mx-auto">
-              <h1 className="mt-20 text-4xl font-extrabold">Shop Ajvara</h1>
-              <img
-                className="h-48 w-96 items-center object-none"
-                src="https://source.unsplash.com/man-in-black-jacket-and-brown-pants-lying-on-snow-covered-ground-during-daytime-q80sx583gzE"
-                alt=""
+    <div className="flex min-h-screen flex-col bg-slate-500">
+      <header className="fixed inset-x-0 top-0">
+        <div className="container mx-auto">
+          <nav>
+            <ul className="flex items-center justify-center gap-3">
+              <li>
+                <StyledLink href="/home">Home</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="/store">Store</StyledLink>
+              </li>
+              <li>
+                <StyledLink href="/page">Page</StyledLink>
+              </li>
+              <ShoppingBag
+                href="/cart"
+                className="absolute right-6 hover:bg-gray-400"
               />
-              <p className="font-medium">
-                Here you can by some ajvar for your brekfast!!
-              </p>
-            </section>
-          </main>
+            </ul>
+          </nav>
         </div>
+      </header>
 
-        <footer className="sticky bottom-0 flex justify-center gap-8">
-          © Nicknamer 2024
-          <a className="hover:underline" href="https://github.com/NicknamerSRB">
-            Github
-          </a>
-        </footer>
-      </div>
-    </>
+      <main className="mt-[32px] grow">
+        <section>
+          <div className="container mx-auto">
+            <h1 className="text-4xl font-extrabold">Shop Ajvara</h1>
+            <img
+              className="h-48 w-96 items-center object-cover"
+              src="https://source.unsplash.com/a-close-up-of-a-red-pepper-with-a-green-stalk-a3coY0CSMy4"
+              alt=""
+            />
+            <p className="font-medium">
+              Here you can by some ajvar for your brekfast!!
+            </p>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <div className="container mx-auto flex justify-center">
+          <p>
+            © Nicknamer 2024
+            <a
+              className="hover:underline"
+              href="https://github.com/NicknamerSRB"
+            >
+              Github
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
   )
 }
 
