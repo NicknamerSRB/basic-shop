526c1236f6f0d22ef00ab915d5398117d3b1169a
nicknamer <nicknamer>
2024-05-15 01:30:30 +0200

    feat: implement fetch filter

--- src/components/pages/ShopPage.tsx
+++ src/components/pages/ShopPage.tsx
@@ -1,14 +1,23 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useQuery } from '@/hooks/useQuery'
+import { Product } from '@/services/products'
 
 const Shop = () => {
   const query = useQuery()
+  const [products, setProducts] = useState<Product[]>([])
 
   useEffect(() => {
-    query.fetch()
+    query.fetch({
+      onSuccess: (products) => {
+        const availableProducts = products.filter(
+          (product) => product.availability,
+        )
+        setProducts(availableProducts)
+      },
+    })
   }, [])
 
-  console.log(query)
+  console.log(products)
 
   return <></>
 }
